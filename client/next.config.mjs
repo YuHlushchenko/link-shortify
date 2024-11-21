72fc63bbbd3958f3eef15ac1487d30ec38b85b71
YuHlushchenko <yuhlushchenko@gmail.com>
2024-11-21 19:34:43 +0200

    Next updated to 15

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,6 +3,13 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./src/app/i18n/request.ts')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    // Use the new Dart Sass compiler
+    // api: 'modern-compiler', // should work, but doesn't
+    silenceDeprecations: ['legacy-js-api'], // Disable deprecation warnings (sass)
+    // TODO: try again when sass is updated
+  },
+}
 
 export default withNextIntl(nextConfig)
--- client/src/app/i18n/request.ts
+++ client/src/app/i18n/request.ts
@@ -1,13 +1,24 @@
-import { notFound } from 'next/navigation'
+// import { notFound } from 'next/navigation'
 import { getRequestConfig } from 'next-intl/server'
 import { routing } from './routing'
+import { TLocale } from '../types/global'
 
-export default getRequestConfig(async ({ locale }) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!routing.locales.includes(locale as 'en' | 'uk')) notFound()
+export default getRequestConfig(
+  async ({
+    // locale
+    requestLocale,
+  }) => {
+    // This typically corresponds to the `[locale]` segment
+    let locale = await requestLocale
 
-  return {
-    messages: (await import(`../../../public/locales/${locale}/common.json`))
-      .default,
-  }
-})
+    // Ensure that the incoming locale is valid
+    if (!locale || !routing.locales.includes(locale as TLocale)) {
+      locale = routing.defaultLocale
+    }
+
+    return {
+      messages: (await import(`../../../public/locales/${locale}/common.json`))
+        .default,
+    }
+  },
+)
