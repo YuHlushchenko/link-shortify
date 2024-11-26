052550383ee94dc2b562b4d1d5f9eaaa27b6d06a
YuHlushchenko <yuhlushchenko@gmail.com>
2024-11-27 01:21:35 +0200

    Loading animation

--- client/src/pages/loading/ui/LoadingPage.tsx
+++ client/src/pages/loading/ui/LoadingPage.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import Image from 'next/image'
+
+import styles from './Loading.module.scss'
+
+import Logo from 'public/assets/svgs/logo.svg'
+
+const CIRCLES_COUNT = 170
 
 const Loading = () => {
-  return <div>Loading</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <Image src={Logo} alt='Link-Shortify' width={35} height={35} />
+        <h1>Link-Shortify</h1>
+      </div>
+
+      {Array.from({ length: CIRCLES_COUNT }).map((_, index) => (
+        <div key={index} className={styles.infinityContainer}>
+          <div
+            className={styles.circleLeft}
+            style={{
+              animationDelay: `${index * 0.01}s`,
+              opacity: 1 - index / CIRCLES_COUNT,
+              backgroundColor: `hsl(${(index * 360) / CIRCLES_COUNT}, 100%, 50%)`,
+              // makes circle ivisible until it's time to animate
+            }}
+          />
+          <div
+            className={styles.circleRight}
+            style={{
+              animationDelay: `${index * 0.01}s`,
+              opacity: 1 - index / CIRCLES_COUNT,
+              backgroundColor: `hsl(${(index * 360) / CIRCLES_COUNT}, 100%, 50%)`,
+            }}
+          />
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Loading
