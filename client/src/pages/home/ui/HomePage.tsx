319f4e8ec1c58ba7501eab0ee885c96dc3bf975d
YuHlushchenko <yuhlushchenko@gmail.com>
2024-11-27 01:21:35 +0200

    Loading animation

--- client/src/pages/home/ui/HomePage.tsx
+++ client/src/pages/home/ui/HomePage.tsx
@@ -1,25 +1,27 @@
-import { useTranslations } from 'next-intl'
+// import { useTranslations } from 'next-intl'
 
-import Button from '@/shared/ui/Button/Button'
-import { ThemeSwitcher } from '@/features/ThemeSwitcher'
-import Heading from '@/shared/ui/Heading/Heading'
+// import Button from '@/shared/ui/Button/Button'
+// import { ThemeSwitcher } from '@/features/ThemeSwitcher'
+// import Heading from '@/shared/ui/Heading/Heading'
 
-import { Link } from '@/app/i18n/routing'
+// import { Link } from '@/app/i18n/routing'
+import { LoadingPage } from '@/pages/loading'
 
 const HomePage = () => {
-  const t = useTranslations('common')
+  // const t = useTranslations('common')
 
   return (
     <>
-      <h1>Home</h1>
+      {/* <h1>Home</h1>
 
       <p>Welcome to the Home page</p>
       <code>{t('title')}</code>
 
       <Button className='default'>btn</Button>
       <Heading>Heading</Heading>
-      <Link href='/about'>About</Link>
-      <ThemeSwitcher />
+      <Link href='/about'>About</Link> */}
+      {/* <ThemeSwitcher /> */}
+      <LoadingPage />
     </>
   )
 }
