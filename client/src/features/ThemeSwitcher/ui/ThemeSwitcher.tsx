ef6706d2a25d5e425c0798c2b85b10e29a772d05
YuHlushchenko <yuhlushchenko@gmail.com>
2024-12-08 17:59:24 +0200

    svgr, theme switcher

--- client/src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ client/src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,13 +1,17 @@
 'use client'
 
-import Button from '@/shared/ui/Button/Button'
+import { useTranslations } from 'next-intl'
 import { Theme } from '@/shared/const/theme'
 import { useStore } from '@/app/providers/store/createStore'
 
 import styles from './ThemeSwitcher.module.scss'
 
+import Sun from 'public/assets/svgs/light-theme-icon.svg'
+import Moon from 'public/assets/svgs/dark-theme-icon.svg'
+
 export const ThemeSwitcher = () => {
   const { theme, setTheme } = useStore()
+  const t = useTranslations('common')
 
   const toggleThemeHandler = () => {
     const newTheme =
@@ -21,11 +25,39 @@ export const ThemeSwitcher = () => {
 
   return (
     <div className={styles.container}>
-      <Button onClick={toggleThemeHandler}>Toggle Theme</Button>
-      <p>
-        Theme:
-        <b>{theme}</b>
-      </p>
+      <div role='button' tabIndex={0} onClick={toggleThemeHandler}>
+        <div
+          className={
+            theme === Theme.LIGHT
+              ? `${styles.lightThemeContainer} ${styles.lightThemeContainerActive}`
+              : `${styles.lightThemeContainer}`
+          }
+        >
+          <div className={styles.iconContainer}>
+            <Sun />
+          </div>
+          <p>
+            {t('light')}
+            {theme === Theme.LIGHT ? ` ${t('theme')}` : ''}
+          </p>
+        </div>
+
+        <div
+          className={
+            theme === Theme.DARK
+              ? `${styles.darkThemeContainer} ${styles.darkThemeContainerActive}`
+              : `${styles.darkThemeContainer}`
+          }
+        >
+          <div className={styles.iconContainer}>
+            <Moon />
+          </div>
+          <p>
+            {t('dark')}
+            {theme === Theme.DARK ? ` ${t('theme')}` : ''}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
