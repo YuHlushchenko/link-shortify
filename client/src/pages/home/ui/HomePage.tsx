29944b7f37364861d9a0ee87bf7b728bc5d2b0eb
YuHlushchenko <yuhlushchenko@gmail.com>
2024-12-08 19:08:29 +0200

    home backrgound has been changed

--- client/src/pages/home/ui/HomePage.tsx
+++ client/src/pages/home/ui/HomePage.tsx
@@ -6,26 +6,26 @@ import { ThemeSwitcher } from '@/features/ThemeSwitcher'
 
 import styles from './HomePage.module.scss'
 
-import CubeOne from 'public/assets/svgs/cube1.svg'
-import CubeTwo from 'public/assets/svgs/cube2.svg?url'
-import CubeThree from 'public/assets/svgs/cube3.svg?url'
+import SwirlUrl from 'public/assets/svgs/swirl.svg?url'
 
 const HomePage = () => {
   // const t = useTranslations('common')
 
   return (
     <div className={styles.container}>
-      <div className={styles.bgSwirlContainer} />
+      <div className={styles.bgSwirlContainer}>
+        <Image priority src={SwirlUrl} alt='swirl' />
+      </div>
+
       <div className={styles.bgCubesContainer}>
         <div className={styles.cubeOneContainer}>
-          {/* <Image priority src={CubeOne} alt='cube1' /> */}
-          <CubeOne />
+          <div className={styles.cubeOne} />
         </div>
         <div className={styles.cubeTwoContainer}>
-          <Image priority src={CubeTwo} alt='cube2' />
+          <div className={styles.cubeTwo} />
         </div>
         <div className={styles.cubeThreeContainer}>
-          <Image priority src={CubeThree} alt='cube3' />
+          <div className={styles.cubeThree} />
         </div>
 
         <div className={styles.themeSwitcherContainer}>
