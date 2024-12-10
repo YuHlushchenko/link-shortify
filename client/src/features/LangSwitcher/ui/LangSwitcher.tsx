5a21d9c1cbd077f2b1a4beca129ef5afa62ea53d
YuHlushchenko <yuhlushchenko@gmail.com>
2024-12-10 21:12:41 +0200

    lang switcher has been added

--- client/src/features/LangSwitcher/ui/LangSwitcher.tsx
+++ client/src/features/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,28 +1,67 @@
 'use client'
 
-// import { useParams } from 'next/navigation'
-// import { ChangeEvent, ReactNode, useTransition } from 'react'
-// import { Link, redirect, usePathname, useRouter } from '@/app/i18n/routing'
+import Image from 'next/image'
+import { useParams } from 'next/navigation'
+import { useTransition } from 'react'
+import { usePathname, useRouter } from '@/app/i18n/routing'
+// import { TLocale } from '@/app/types/global'
 
 import styles from './LangSwitcher.module.scss'
 
+import UAFlag from 'public/assets/svgs/ua-flag.svg?url'
+import ENFlag from 'public/assets/svgs/en-flag.svg?url'
+
 const LangSwitcher = () => {
-  //   const router = useRouter()
-  //   const pathname = usePathname()
-  //   const params = useParams()
-  //   const [isPending, startTransition] = useTransition()
+  const router = useRouter()
+  const pathname = usePathname()
+  const params = useParams()
+  const [isPending, startTransition] = useTransition()
 
   const toggleLangHandler = () => {
-    // const nextLocale = router.locale === 'en' ? 'uk' : 'en'
-    // startTransition(() => {
-    //   router.replace({ pathname, params }, { locale: nextLocale })
-    // })
+    const nextLocale = params?.locale === 'uk' ? 'en' : 'uk'
+
+    startTransition(() => {
+      router.replace({ pathname }, { locale: nextLocale })
+    })
   }
 
-  return (
-    <div className={styles.container}>
-      <div role='button' tabIndex={0} onClick={toggleLangHandler}>
-        LangSwitcher
+  return isPending ? (
+    <div>Loading...</div>
+  ) : (
+    <div
+      className={styles.container}
+      role='button'
+      tabIndex={0}
+      onClick={toggleLangHandler}
+    >
+      <div className={styles.langSwitcherBtn}>
+        <div
+          className={
+            params?.locale === 'uk'
+              ? `${styles.langContainer} ${styles.langContainerActive}`
+              : styles.langContainer
+          }
+        >
+          <div className={styles.imgContainer}>
+            <Image src={UAFlag} alt='ukrainian' />
+          </div>
+
+          <p>ua</p>
+        </div>
+
+        <div
+          className={
+            params?.locale === 'en'
+              ? `${styles.langContainer} ${styles.langContainerActive}`
+              : styles.langContainer
+          }
+        >
+          <div className={styles.imgContainer}>
+            <Image src={ENFlag} alt='english' />
+          </div>
+
+          <p>en</p>
+        </div>
       </div>
     </div>
   )
