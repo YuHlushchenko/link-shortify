b66650641ec3e9223e3fc9f6baf3ae0e74868770
YuHlushchenko <yuhlushchenko@gmail.com>
2025-03-28 20:55:22 +0200

    husky fixed, store updated

---
+++ client/src/features/InputLinkWithAutoPaste/store/useAutoPasteStore.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand'
+
+// allows pushing new value without spreading the state (state) => ({ ...state.users, name: username })
+// just use set(state => state.users.push({ name: username }))
+import { immer } from 'zustand/middleware/immer'
+import {
+  devtools,
+  // persist // persist the store in localstorage
+} from 'zustand/middleware' // devtools work with redux devtools
+
+import createSelectors from './selectors'
+
+interface IAutoPasteStore {
+  isAutoPaste: boolean
+  setAutoPaste: (isAutoPaste: boolean) => void
+}
+
+const useAutoPasteStore = create<IAutoPasteStore>()(
+  //   persist(
+  devtools(
+    immer((set) => ({
+      isAutoPaste: false,
+      setAutoPaste: (newAutoPaste) =>
+        set(() => ({ isAutoPaste: newAutoPaste })),
+    })),
+  ),
+  // { name: 'autoPaste', version: 1 },
+)
+// )
+
+export default createSelectors(useAutoPasteStore)
