e99ca4ff71fdd097f88ccefbde835ce4e4746fbe
YuHlushchenko <yuhlushchenko@gmail.com>
2026-06-10 02:53:57 +0300

    fix(client): format with prettier

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -11,54 +11,70 @@ const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
+  allConfig: js.configs.all,
 })
 
-export default defineConfig([globalIgnores(['**/storybook-static', '.loki', 'config/.storybook/**']), {
-  extends: [
-    ...nextCoreWebVitals,
-    ...nextTypescript,
-    ...compat.extends('prettier'),
-    ...compat.extends('plugin:storybook/recommended')
-  ],
+export default defineConfig([
+  globalIgnores(['**/storybook-static', '.loki', 'config/.storybook/**']),
+  {
+    extends: [
+      ...nextCoreWebVitals,
+      ...nextTypescript,
+      ...compat.extends('prettier'),
+      ...compat.extends('plugin:storybook/recommended'),
+    ],
 
-  rules: {
-    'react-hooks/exhaustive-deps': 'off',
-    'react/react-in-jsx-scope': 'off',
-    indent: [1, 2],
-    quotes: [1, 'single'],
-    semi: [1, 'never'],
+    rules: {
+      'react-hooks/exhaustive-deps': 'off',
+      'react/react-in-jsx-scope': 'off',
+      indent: [1, 2],
+      quotes: [1, 'single'],
+      semi: [1, 'never'],
 
-    'max-len': [1, 80, {
-      ignoreComments: true,
-      ignoreTrailingComments: true,
-      ignoreUrls: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-      ignoreRegExpLiterals: true,
-      ignorePattern: '^\\s*var\\s.+=\\s*require\\s*\\(',
-    }],
+      'max-len': [
+        1,
+        80,
+        {
+          ignoreComments: true,
+          ignoreTrailingComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true,
+          ignorePattern: '^\\s*var\\s.+=\\s*require\\s*\\(',
+        },
+      ],
 
-    'arrow-parens': [1, 'always'],
-    'object-curly-spacing': [1, 'always'],
-    'array-bracket-spacing': [1, 'never'],
+      'arrow-parens': [1, 'always'],
+      'object-curly-spacing': [1, 'always'],
+      'array-bracket-spacing': [1, 'never'],
 
-    'no-multiple-empty-lines': [1, {
-      max: 1,
-    }],
+      'no-multiple-empty-lines': [
+        1,
+        {
+          max: 1,
+        },
+      ],
 
-    'no-trailing-spaces': [1],
+      'no-trailing-spaces': [1],
 
-    'no-unused-vars': [1, {
-      args: 'none',
-    }],
+      'no-unused-vars': [
+        1,
+        {
+          args: 'none',
+        },
+      ],
 
-    'no-console': [1],
+      'no-console': [1],
 
-    'react/jsx-one-expression-per-line': [1, {
-      allow: 'single-child',
-    }],
+      'react/jsx-one-expression-per-line': [
+        1,
+        {
+          allow: 'single-child',
+        },
+      ],
 
-    '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
   },
-}])
+])
